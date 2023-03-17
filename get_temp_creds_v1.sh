81fcd55ac55e68b3b5141f51f4faba716d7e6dc6
ceverson-ilmn <109386299+ceverson-ilmn@users.noreply.github.com>
2023-03-17 13:01:46 -0700

    Update get_temp_creds_v1.sh

--- get_temp_creds_v1.sh
+++ get_temp_creds_v1.sh
@@ -12,6 +12,8 @@ DESTINATION_DATA_ID=$5
 #Get the DESTINATION API Key
 DESTINATION_API_KEY=$6
 
+date > output.txt
+echo "Args successfully submitted" >> output.txt
 
 #Make the SOURCE API request for temp storage access credentials
 SOURCE_API_REQ=$(curl -sX 'POST' \
@@ -20,6 +22,9 @@ SOURCE_API_REQ=$(curl -sX 'POST' \
   -H 'X-API-Key: '$SOURCE_API_KEY'' \
   -H 'Content-Type: application/vnd.illumina.v3+json' \
   -d '{"credentialsFormat": "RCLONE"}')
+
+date >> output.txt
+echo "Source request submitted" >> output.txt
 
 #Parse the SOURCE KeyID
 SOURCE_KEY_ID=$(sed 's/.*access_key_id\"\:\"//g' <<< $SOURCE_API_REQ)
@@ -37,6 +42,9 @@ SOURCE_SESSION_TOKEN=$(sed 's/\"\,\"server_side_encryption.*//g' <<< $SOURCE_SES
 SOURCE_FILEPATH=$(sed 's/.*filePathPrefix\"\:\"//g' <<< $SOURCE_API_REQ)
 SOURCE_FILEPATH=$(sed 's/\"\,\"storageType.*//g' <<< $SOURCE_FILEPATH)
 
+date >> output.txt
+echo "Source request parsed" >> output.txt
+
 #Make the SOURCE API request for temp storage access credentials
 DESTINATION_API_REQ=$(curl -sX 'POST' \
   'https://ica.illumina.com/ica/rest/api/projects/'$DESTINATION_PROJECT_ID'/data/'$DESTINATION_DATA_ID':createTemporaryCredentials' \
@@ -44,6 +52,10 @@ DESTINATION_API_REQ=$(curl -sX 'POST' \
   -H 'X-API-Key: '$DESTINATION_API_KEY'' \
   -H 'Content-Type: application/vnd.illumina.v3+json' \
   -d '{"credentialsFormat": "RCLONE"}')
+
+date >> output.txt
+echo "Destination request submitted" >> output.txt
+
 #Parse the SOURCE KeyID
 DESTINATION_KEY_ID=$(sed 's/.*access_key_id\"\:\"//g' <<< $DESTINATION_API_REQ)
 DESTINATION_KEY_ID=$(sed 's/\"\,\"session_token.*//g' <<< $DESTINATION_KEY_ID)
@@ -58,19 +70,34 @@ DESTINATION_SESSION_TOKEN=$(sed 's/\"\,\"server_side_encryption.*//g' <<< $DESTI
 DESTINATION_FILEPATH=$(sed 's/.*filePathPrefix\"\:\"//g' <<< $DESTINATION_API_REQ)
 DESTINATION_FILEPATH=$(sed 's/\"\,\"storageType.*//g' <<< $DESTINATION_FILEPATH)
 
+date >> output.txt
+echo "Destination request parsed" >> output.txt
+
 #Construct the Rclone Config File for SOURCE
 printf "[SOURCE]\ntype = s3\nprovider = AWS\naccess_key_id = "$SOURCE_KEY_ID"\nsecret_access_key = "$SOURCE_ACCESS_KEY"\n" > rclone_config.tmp
 printf "region = us-east-1\nserver_side_encryption = AES256\nsession_token = "$SOURCE_SESSION_TOKEN"\nno_check_bucket = true" >> rclone_config.tmp
+
+date >> output.txt
+echo "Constructed rclone file for Source" >> output.txt
 
 #Construct the Rclone Config File for DESTINATION
 printf "\n\n[DESTINATION]\ntype = s3\nprovider = AWS\naccess_key_id = "$DESTINATION_KEY_ID"\nsecret_access_key = "$DESTINATION_ACCESS_KEY"\n" >> rclone_config.tmp
 printf "region = us-east-1\nserver_side_encryption = AES256\nsession_token = "$DESTINATION_SESSION_TOKEN"\nno_check_bucket = true" >> rclone_config.tmp
 
+date >> output.txt
+echo "Constructed rclone file for Destination" >> output.txt
+
 #Construct final command
 FINAL_COMMAND="rclone -P --config rclone_config.tmp sync SOURCE:/\"$SOURCE_FILEPATH\" DESTINATION:/\"$DESTINATION_FILEPATH\""
 
+date >> output.txt
+echo "Final command constructed" >> output.txt
+
 #Run final command
 eval "$FINAL_COMMAND"
+
+date >> output.txt
+echo "Final command submitted" >> output.txt
 
 #Done
 exit
