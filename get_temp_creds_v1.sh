80ce14d133aa7445c648ec01c5396dcd0fac4b2c
ceverson-ilmn <109386299+ceverson-ilmn@users.noreply.github.com>
2023-03-17 08:11:15 -0700

    Create get_temp_creds_v1.sh

---
+++ get_temp_creds_v1.sh
@@ -0,0 +1,76 @@
+#Get the SOURCE ProjectID
+SOURCE_PROJECT_ID=$1
+#Get the SOURCE DataID
+SOURCE_DATA_ID=$2
+#Get the SOURCE API Key
+SOURCE_API_KEY=$3
+
+#Get the DESTINATION ProjectID
+DESTINATION_PROJECT_ID=$4
+#Get the DESTINATION DataID
+DESTINATION_DATA_ID=$5
+#Get the DESTINATION API Key
+DESTINATION_API_KEY=$6
+
+
+#Make the SOURCE API request for temp storage access credentials
+SOURCE_API_REQ=$(curl -sX 'POST' \
+  'https://ica.illumina.com/ica/rest/api/projects/'$SOURCE_PROJECT_ID'/data/'$SOURCE_DATA_ID':createTemporaryCredentials' \
+  -H 'accept: application/vnd.illumina.v3+json' \
+  -H 'X-API-Key: '$SOURCE_API_KEY'' \
+  -H 'Content-Type: application/vnd.illumina.v3+json' \
+  -d '{"credentialsFormat": "RCLONE"}')
+
+#Parse the SOURCE KeyID
+SOURCE_KEY_ID=$(sed 's/.*access_key_id\"\:\"//g' <<< $SOURCE_API_REQ)
+SOURCE_KEY_ID=$(sed 's/\"\,\"session_token.*//g' <<< $SOURCE_KEY_ID)
+
+#Parse the SOURCE Access Key
+SOURCE_ACCESS_KEY=$(sed 's/.*secret_access_key\"\:\"//g' <<< $SOURCE_API_REQ)
+SOURCE_ACCESS_KEY=$(sed 's/\"\,\"no_check_bucket.*//g' <<< $SOURCE_ACCESS_KEY)
+
+#Parse the SOURCE Session Token
+SOURCE_SESSION_TOKEN=$(sed 's/.*session_token\"\:\"//g' <<< $SOURCE_API_REQ)
+SOURCE_SESSION_TOKEN=$(sed 's/\"\,\"server_side_encryption.*//g' <<< $SOURCE_SESSION_TOKEN)
+
+#Parse the SOURCE filePathPrefix
+SOURCE_FILEPATH=$(sed 's/.*filePathPrefix\"\:\"//g' <<< $SOURCE_API_REQ)
+SOURCE_FILEPATH=$(sed 's/\"\,\"storageType.*//g' <<< $SOURCE_FILEPATH)
+
+#Make the SOURCE API request for temp storage access credentials
+DESTINATION_API_REQ=$(curl -sX 'POST' \
+  'https://ica.illumina.com/ica/rest/api/projects/'$DESTINATION_PROJECT_ID'/data/'$DESTINATION_DATA_ID':createTemporaryCredentials' \
+  -H 'accept: application/vnd.illumina.v3+json' \
+  -H 'X-API-Key: '$DESTINATION_API_KEY'' \
+  -H 'Content-Type: application/vnd.illumina.v3+json' \
+  -d '{"credentialsFormat": "RCLONE"}')
+#Parse the SOURCE KeyID
+DESTINATION_KEY_ID=$(sed 's/.*access_key_id\"\:\"//g' <<< $DESTINATION_API_REQ)
+DESTINATION_KEY_ID=$(sed 's/\"\,\"session_token.*//g' <<< $DESTINATION_KEY_ID)
+#Parse the SOURCE Access Key
+DESTINATION_ACCESS_KEY=$(sed 's/.*secret_access_key\"\:\"//g' <<< $DESTINATION_API_REQ)
+DESTINATION_ACCESS_KEY=$(sed 's/\"\,\"no_check_bucket.*//g' <<< $DESTINATION_ACCESS_KEY)
+#Parse the SOURCE Session Token
+DESTINATION_SESSION_TOKEN=$(sed 's/.*session_token\"\:\"//g' <<< $DESTINATION_API_REQ)
+DESTINATION_SESSION_TOKEN=$(sed 's/\"\,\"server_side_encryption.*//g' <<< $DESTINATION_SESSION_TOKEN)
+#Parse the SOURCE filePathPrefix                                                                                                                                
+                                     
+DESTINATION_FILEPATH=$(sed 's/.*filePathPrefix\"\:\"//g' <<< $DESTINATION_API_REQ)
+DESTINATION_FILEPATH=$(sed 's/\"\,\"storageType.*//g' <<< $DESTINATION_FILEPATH)
+
+#Construct the Rclone Config File for SOURCE
+printf "[SOURCE]\ntype = s3\nprovider = AWS\naccess_key_id = "$SOURCE_KEY_ID"\nsecret_access_key = "$SOURCE_ACCESS_KEY"\n" > rclone_config.tmp
+printf "region = us-east-1\nserver_side_encryption = AES256\nsession_token = "$SOURCE_SESSION_TOKEN"\nno_check_bucket = true" >> rclone_config.tmp
+
+#Construct the Rclone Config File for DESTINATION
+printf "\n\n[DESTINATION]\ntype = s3\nprovider = AWS\naccess_key_id = "$DESTINATION_KEY_ID"\nsecret_access_key = "$DESTINATION_ACCESS_KEY"\n" >> rclone_config.tmp
+printf "region = us-east-1\nserver_side_encryption = AES256\nsession_token = "$DESTINATION_SESSION_TOKEN"\nno_check_bucket = true" >> rclone_config.tmp
+
+#Construct final command
+FINAL_COMMAND="rclone -P --config rclone_config.tmp sync SOURCE:/\"$SOURCE_FILEPATH\" DESTINATION:/\"$DESTINATION_FILEPATH\""
+
+#Run final command
+eval "$FINAL_COMMAND"
+
+#Done
+exit
